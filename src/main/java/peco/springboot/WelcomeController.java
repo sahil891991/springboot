b22ad3d7f16b842fcd03152f0057934d91416513
Sahil Sharma <sahil.sharma@bizmerlin.com>
2021-02-19 10:36:40 +0530

    First commit

---
+++ src/main/java/peco/springboot/WelcomeController.java
@@ -0,0 +1,22 @@
+package peco.springboot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class WelcomeController {
+
+	Logger LOGGER=LoggerFactory.getLogger(WelcomeController.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeController.class, args);
+		
+	}
+	@RequestMapping("/welcome")
+	public String welcome() {
+		return "Welcome to spring boot";
+		
+	}
+}
